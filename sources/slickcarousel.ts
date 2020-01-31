cc605e0c812d8149dd89534fdf42df51c327150f
Serge van den Oever <Serge.van.den.Oever@macaw.nl>
2020-01-31 14:55:52 +0100

    components added

---
+++ sources/slickcarousel.ts
@@ -0,0 +1,6 @@
+import "slick-carousel";
+import * as $ from 'jquery';
+//(window as any).$ = (window as any).jQuery = jQuery;
+export function slickcarousel() {
+    $('#slickcarousel').slick();
+}
