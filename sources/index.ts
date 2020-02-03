c5656b447d9e93515abed0e395df2a1ba94e84ba
Serge van den Oever <serge.van.den.oever@macaw.nl>
2020-02-03 01:10:57 +0100

    cleanup

--- sources/index.ts
+++ sources/index.ts
@@ -1,16 +1,12 @@
-// Ensure that jQuery is available a window.$ and window.jQuery
-//import jQuery from 'jquery';
-//(window as any).$ = (window as any).jQuery = jQuery;
-
 // jQuery is already loaded and available on the window object, alias $ as well
 (window as any).$ = (window as any).jQuery;
 
 import "./index.scss";
+import './components/xaclock/xaclock';
 
 import { mylatte } from './components/mylatte/mylatte';
 import { slickcarousel } from './components/slickcarousel/slickcarousel'; 
 import { liveclock } from './components/liveclock/liveclock';
-import './components/xaclock/xaclock';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOCUMENT READY");
